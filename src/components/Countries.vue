f0f70577477bb800b4e677038c7d1fcb8dc42713
sosakpanha <www.menhsosakpanha@gmail.com>
2024-02-23 22:39:25 -0800

    finish fetch all req key to display to component

--- src/components/Countries.vue
+++ src/components/Countries.vue
@@ -7,9 +7,20 @@ import Country from './Country.vue';
     <div class="row justify-content-center">
       <div v-if="filteredData">
         <div v-for="(country, index) in filteredData" :key="index">
-          <Country>
-            <template #icon/>
-            <h2>{{ country.name }}</h2>
+          <Country :flagUrl="country.flag"
+          :nativeNames="country.nativeNames">
+            <h2>Country Name: {{ country.name }}</h2>
+            <h2>2 Character Country Code: {{ country.cca2 }}</h2>
+            <h2>3 Character Country Code:: {{ country.cca3 }}</h2>
+            <div v-if="nativeNames">
+              <div v-for="(nName, index) in nativeNames" :key="index"> 
+                <h2>Native Name: {{ nName.languageCode }}</h2>
+                <h2>Native Name Official: {{ nName.official }}</h2>
+                <h2>Native Name Common: {{ nName.common }}</h2>
+              </div>
+            </div>
+            <h2>Alternative Country Name: {{ country.name }}</h2>
+            <h2>Country Calling Code: {{ country.name }}</h2>
           </Country>
         </div>
       </div>
@@ -31,8 +42,19 @@ export default {
       .then(response => {
         const countries = response.data.map(country => {
           return {
+            flag: country.flags.png,
             name: country.name.official,
-           
+            cca2: country.cca2,
+            cca3: country.cca3,
+            nativeNames: country.name.nativeName ? Object.keys(country.name.nativeName).map(key => {
+              return {
+                languageCode: key,
+                official: country.name.nativeName[key].official,
+                common: country.name.nativeName[key].common
+              };
+            }) : "this country have no native name",
+            altSpellings: country.altSpellings,
+            idd: country.idd
           };
         });
         this.filteredData = countries;
--- src/components/Country.vue
+++ src/components/Country.vue
@@ -1,11 +1,51 @@
 <template>
-        <div class="col-md-4 bg-secondary mx-auto">
-            <div class="card ">
-                <slot name="icon"></slot>
-                <div class="card-body">
-                    <slot name="heading text-black"></slot>
-                     <slot></slot>
+    <div class="col-md-4">
+        <div class="card ">
+            <img :src="flagUrl" alt="Country Flag"/>
+            <div class="card-body">
+                <slot></slot>
+                <div v-if="Array.isArray(nativeNames)">
+                    <div v-for="(nName, index) in nativeNames" :key="index">
+                        <h2>Native Name key: {{ nName.languageCode }}</h2>
+                        <h2>Official: {{ nName.official }}</h2>
+                        <h2>Common: {{ nName.common }}</h2>
+                    </div>
+                </div>
+                <div v-else>
+                    <h2>{{ nativeNames }}</h2>
                 </div>
             </div>
         </div>
+    </div>
 </template>
+
+<script>
+    export default {
+    props: {
+        flagUrl: {
+        type: String,
+        required: true,
+        },
+    cca2: {
+      type: String,
+      required: false,
+    },
+    cca3: {
+      type: String,
+      required: false,
+    },
+    nativeNames: {
+      type: [Array, String], 
+      required: false,
+    },
+    altSpellings: {
+      type: Array,
+      required: false,
+    },
+    idd: {
+      type: Object,
+      required: false,
+    },
+    },
+    };
+</script>
